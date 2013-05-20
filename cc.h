aed737276e2639f35dceccc4348421d1b63a2427
Sean Young <sean@mess.org>
2013-05-20 11:17:40 +0100

    1 minute is far too small. Set to half an hour(!)

--- cc.h
+++ cc.h
@@ -3,7 +3,7 @@
 #define __CC_H__
 
 #define CC_DEVICE "/dev/ttyUSB0"
-#define CC_TIMEOUT 60
+#define CC_TIMEOUT (60*30)
 
 struct currentcost {
 	void (*cb)(double temperature, unsigned channels, unsigned *watts);
